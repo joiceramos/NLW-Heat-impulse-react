4e71ec96c99667af2908279a4c456cf443dc1f80
joiceramos <joice.ramos@sevensys.tech>
2021-11-25 21:05:21 -0300

    app complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useContext } from 'react';
+import styles from './App.module.scss';
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './contexts/auth';
+
 export function App() {
 
+  const { user } = useContext(AuthContext);
+
   return (
-    <h1>Hello World!</h1>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      { !!user ? <SendMessageForm/> : <LoginBox /> }
+    </main>
   )
+  //48:29
 }
